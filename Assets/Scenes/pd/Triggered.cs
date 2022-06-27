5920a6f11ffc54d862e6f11d1cebc9ae74edbc86
IrwinTaken <irwin.ladrette@etu.univ-smb.fr>
2022-06-27 17:51:16 +0200

    Revert "Merge branch 'main' of https://github.com/Nelda74/Jeu-Unity"
    This reverts commit 72d8b0ce636e1c348f10077c0ac4aec46cab8a6c, reversing
    changes made to 221628cb5560cce88196381802809faf200d7046.

---
+++ Assets/Scenes/pd/Triggered.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Triggered : MonoBehaviour
+{
+    
+
+    public float speed;
+    public Transform door;
+    public float maxOpenValue;
+    public bool oppening = false;
+    public bool closing = false;
+    public float currentValue = 0;
+
+
+    void Start()
+    {
+        
+    }
+    // Start is called before the first frame update
+    void Update()
+    {
+
+            if (oppening == true)
+            {
+                OpenDoor();
+            }
+            if (closing == true)
+            {
+                CloseDoor();
+            }
+        
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+
+        oppening = true;
+        Debug.Log("yayayayyaa");
+       
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+
+        if (other.transform.name == "Player1")
+        {
+            oppening = false;
+            closing = true;
+        }
+    }
+
+    void OpenDoor()
+    {
+        float movement = speed * Time.deltaTime;
+        currentValue += movement;
+
+        if (currentValue <= maxOpenValue)
+        {
+            door.position = new Vector3(door.position.x , door.position.y - movement, door.position.z);
+        }
+        else
+        {
+            oppening = false;
+        }
+
+    }
+
+    void CloseDoor()
+    {
+        float movement = speed * Time.deltaTime;
+        currentValue -= movement;
+
+        if (currentValue >= 0)
+        {
+            door.position = new Vector3(door.position.x, door.position.y + movement, door.position.z);
+        }
+        else
+        {
+            closing = false;
+        }
+
+    }
+
+
+
+
+}
