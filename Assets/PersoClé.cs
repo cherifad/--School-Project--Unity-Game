221628cb5560cce88196381802809faf200d7046
IrwinTaken <irwin.ladrette@etu.univ-smb.fr>
2022-06-27 17:47:13 +0200

    switch touch cle

---
+++ Assets/PersoClé.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PersoClé : MonoBehaviour
+{
+    public GameObject key;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag.Equals("key"))
+        {
+            SceneManager.LoadScene("KeyGrabe");
+            key.SetActive(false);
+        }
+
+    }
+    
+}
---
+++ Assets/Wait.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Wait : MonoBehaviour
+{
+    void Start()
+    {
+
+        // Démarre la fonction Wait (coroutine)
+        // Wait veut dire attendre
+        StartCoroutine(Wait2());
+
+        
+    }
+
+    private IEnumerator Wait2()
+    {
+        yield return new WaitForSeconds(8f);
+
+        SceneManager.LoadScene("SampleScene");
+    }
+
+}
