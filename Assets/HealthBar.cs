e97e5802535ab22e8b8975b097b5c8e20d0492e7
Nelda74 <95074031+Nelda74@users.noreply.github.com>
2022-06-28 09:44:21 +0200

    health

---
+++ Assets/HealthBar.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBar : MonoBehaviour
+{
+    private static Image HealthBarImage;
+
+    /// <summary>
+    /// Sets the health bar value
+    /// </summary>
+    /// <param name="value">should be between 0 to 1</param>
+    public static void SetHealthBarValue(float value)
+    {
+        HealthBarImage.fillAmount = value;
+        if (HealthBarImage.fillAmount < 0.2f)
+        {
+            SetHealthBarColor(Color.red);
+        }
+        else if (HealthBarImage.fillAmount < 0.4f)
+        {
+            SetHealthBarColor(Color.yellow);
+        }
+        else
+        {
+            SetHealthBarColor(Color.green);
+        }
+    }
+
+    public static float GetHealthBarValue()
+    {
+        return HealthBarImage.fillAmount;
+    }
+
+    /// <summary>
+    /// Sets the health bar color
+    /// </summary>
+    /// <param name="healthColor">Color </param>
+    public static void SetHealthBarColor(Color healthColor)
+    {
+        HealthBarImage.color = healthColor;
+    }
+
+    /// <summary>
+    /// Initialize the variable
+    /// </summary>
+    private void Start()
+    {
+        HealthBarImage = GetComponent<Image>();
+    }
+}
